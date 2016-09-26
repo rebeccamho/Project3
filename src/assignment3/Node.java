90d463dd0c65f225d3e6a5f0ca38c81f4510af72
Rebecca Ho <rebeccaho936@gmail.com>
2016-09-26 18:07:31 -0500

    first commit

---
+++ src/assignment3/Node.java
@@ -0,0 +1,13 @@
+package assignment3;
+
+import java.util.ArrayList;
+
+public class Node {
+	ArrayList<Node> children = new ArrayList<Node>();
+	Node parent;
+	String word;
+	
+	Node(Node parent) {
+		this.parent = parent;
+	}
+}
