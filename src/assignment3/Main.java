4113ca2b39ffdaad8ea00062ffd8d8c49350448a
Rebecca Ho <rebeccaho936@gmail.com>
2016-09-29 14:09:37 -0500

    added header

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -1,14 +1,13 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
- * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
- * Slip days used: <0>
- * Git URL:
+ * Ben Zimmerman
+ * bdz223
+ * 16465
+ * Rebecca Ho
+ * rh29645
+ * 16480
+ * Slip days used: 0
+ * Git URL: https://github.com/rebeccamho/Project3
  * Fall 2016
  */
 
--- src/assignment3/Node.java
+++ src/assignment3/Node.java
@@ -1,3 +1,16 @@
+/* WORD LADDER Node.java
+ * EE422C Project 3 submission by
+ * Ben Zimmerman
+ * bdz223
+ * 16465
+ * Rebecca Ho
+ * rh29645
+ * 16480
+ * Slip days used: 0
+ * Git URL: https://github.com/rebeccamho/Project3
+ * Fall 2016
+ */
+
 package assignment3;
 
 import java.util.ArrayList;
